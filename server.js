dbdfd8d89d8be6d7875c8445226542d18d807afb
neeta17 <01fe19bcs195@kletech.ac.in>
2022-06-19 16:02:28 +0530

    authors/index.ejs

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ if(process.env.NODE_ENV !=='production')
 const express=require('express')
 const app=express()
 const expresslayouts=require('express-ejs-layouts')
+
 const indexRouter=require('./routes/index')
+const authorRouter=require('./routes/authors')
+
 app.set('views','./views')
 app.set('view engine','ejs')
-
 
 app.set('layout','layouts/layout')
 app.use(expresslayouts)
@@ -18,7 +20,11 @@ app.use(express.static('public'))
 const mongoose=require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 const db=mongoose.connection
+
 db.on('error',error=> console.error(error))
 db.on('open',()=> console.log('Connected to mongoose'))
+
 app.use('/',indexRouter)
-app.listen(process.env.ORT|| 3000)
+app.use('/authors',authorRouter)
+
+app.listen(process.env.PORT|| 3000)
